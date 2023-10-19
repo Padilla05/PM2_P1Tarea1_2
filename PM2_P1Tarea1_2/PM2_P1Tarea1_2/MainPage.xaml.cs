f1fafb7e302263815dce051d23c4aaf788fff569
Alejandra <erikavalladares2011@hotmail.com>
2023-10-18 19:25:38 -0600

    Agregar archivos de proyecto.

---
+++ PM2_P1Tarea1_2/PM2_P1Tarea1_2/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2_P1Tarea1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrar_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
